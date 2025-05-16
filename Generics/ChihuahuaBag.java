6fdcfa3a927396dfa46ef4fed238371cedf0989d
sarahfarris <sarahfarris45@gmail.com>
2025-05-16 13:00:44 -0700

    reorganizing files 5-16

--- Generics/ChihuahuaBag.java
+++ Generics/ChihuahuaBag.java
@@ -6,7 +6,21 @@ public class ChihuahuaBag {
 
     List<Chihuahua> items;
 
-    public void add(Chihuahua chihuahua){}
+    public ChihuahuaBag(List<Chihuahua> items) {
+        this.items = items;
+    }
+
+    public List<Chihuahua> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Chihuahua> items) {
+        this.items = items;
+    }
+
+    public void add(Chihuahua chihuahua){
+        this.items.add(chihuahua);
+    }
 
 
     public void printContents(){
